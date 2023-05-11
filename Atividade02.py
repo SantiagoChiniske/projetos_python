65420d3b3ae206b4916fedbd03f3ebddb4a1a4f4
santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-05-11 15:00:14 -0300

    Tabela de preço e valores do produtos

---
+++ Atividade02.py
@@ -0,0 +1,20 @@
+print('Bem vindo a Lanchonete do Santiago Chiniske Pereira')
+print(28*'*'+'Cardápio'+27*'*')
+print('|'+4*' '+'Código'+ 3*' ' +'|' +8*' '+'Descrição'+20*' '+'|'+2*' '+'Valor'+2*' '+'|')
+print('|'+5*' '+'100'+ 5*' ' +'|' +8*' '+'Cachorro Quente'+14*' '+'|'+2*' '+'9,00'+3*' '+'|')
+print('|'+5*' '+'101'+ 5*' ' +'|' +8*' '+'Cachorro Quente Duplo'+8*' '+'|'+2*' '+'11,00'+2*' '+'|')
+print('|'+5*' '+'102'+ 5*' ' +'|' +8*' '+'X-Egg'+24*' '+'|'+2*' '+'12,00'+2*' '+'|')
+print('|'+5*' '+'103'+ 5*' ' +'|' +8*' '+'X-Salada'+21*' '+'|'+2*' '+'12,00'+2*' '+'|')
+print('|'+5*' '+'104'+ 5*' ' +'|' +8*' '+'X-Bacon'+22*' '+'|'+2*' '+'14,00'+2*' '+'|')
+print('|'+5*' '+'105'+ 5*' ' +'|' +8*' '+'X-Tudo'+23*' '+'|'+2*' '+'17,00'+2*' '+'|')
+print('|'+5*' '+'200'+ 5*' ' +'|' +8*' '+'Refrigerenta Lata'+12*' '+'|'+2*' '+'5,00'+3*' '+'|')
+print('|'+5*' '+'210'+ 5*' ' +'|' +8*' '+'Chá Gelado'+19*' '+'|'+2*' '+'4,00'+3*' '+'|')
+
+valor_cachorroQuente = 9.00
+valor_cachorroQuenteDuplo = 11.00
+valor_xEgg = 12.00
+valor_xSalada = 12.00
+valor_xBacon = 14.00
+valor_xTudo = 17.00
+valor_refrigeranteLata = 5.00
+valor_chaGelado = 4.00
