38791c4b5e5641aabf4ce0b1cb0262f7cd579959
santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-05-21 13:19:44 -0300

    Criando as funçoes e o main

---
+++ Atividade04.py
@@ -0,0 +1,49 @@
+#Atividade 04
+
+#------Início das variáveis globais-----
+lista_pecas = []
+codigo_pecas = 0
+#------Fim das variáveis globais-----
+
+
+#------Início das Cadastrar peça-----
+def cadastrar_peca(codigo):
+    ...
+#------Fim das Cadastrar peça-----
+
+#------Início das Consulta peça-----
+def consulta_peca():
+    ...
+#------Fim das Consulta peça-----
+
+#------Início das Remover peça-----
+def remover_peca():
+    ...
+#------Fim das Remover peça-----
+
+#------Início do main-----
+print('Bem vindo ao Controle de Estoque da bicicletaria do Santiago Chiniske Pereira')
+while True:
+    opcao_principal =input('\nEscolha a opção desejada: \n'+
+                            '1- Cadastra Peças \n'+
+                            '2- Consultar Peças\n'+
+                            '3- Remover Peças\n'+
+                            '4-Sair \n'+
+                            '>>')
+    if opcao_principal == '1':
+            codigo_pecas = codigo_pecas + 1
+            cadastrar_peca(codigo_pecas)
+
+    elif opcao_principal == '2':
+        consulta_peca()
+
+    elif opcao_principal == '3':
+        remover_peca()
+
+    elif opcao_principal == '4':
+        break
+
+    else:
+        print('Opção inválida. tente novamente')
+        continue
+#------Fim do main-----
